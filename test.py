7c28480b9fc9a3722c4f0b012f5db43a76ffac80
varunkumar-annam <varunkumarannam@gmail.com>
2025-01-27 18:26:03 +0530

    new html files

--- test.py
+++ test.py
@@ -100,7 +100,7 @@ def face_check():
 
         try:
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-            print("madhan")
+            # print("madhan")
             # Pass face to prediction model\n",
             # \"results\" comprises of a tuple containing the label and the confidence value\n",
             results = model.predict(face)
@@ -112,13 +112,13 @@ def face_check():
             cv2.putText(image, display_string, (100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 120, 150), 2)
 
             if confidence >= 85:
-                print(confidence)
+                # print(confidence)
                 cv2.putText(image, "Unlocked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
                 return 1
             else:
                 cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
                 cv2.imshow('Face Recognition', image)
-                print(confidence)
+                # print(confidence)
                 return 2
 
 
@@ -128,6 +128,7 @@ def face_check():
             cv2.imshow('Face Recognition', image)
             print('image not found')
             return 0
+
 
 def blink():
     cap = cv2.VideoCapture(0)
@@ -196,18 +197,21 @@ def blink():
             else:
                 fin_key += str(blinkCounter)
             blinkCounter = 0
-        print(fin_key)
+        # print(fin_key)
         if len(fin_key) == len(pass_word):
             break
         cv2.imshow("Image", imgStack)
         cv2.waitKey(25)
-    print(tm)
+    # print(tm)
     return fin_key
 
 
 
 
+
+
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template("home.html")
 
@@ -227,7 +231,7 @@ def reset():
             f.write(k)
             f.close()
     flash("oops! something went wrong try again!")
-    return render_template('dashboard.html')
+    return render_template('auth_sucess.html')
 @app.route('/register')
 def register():
     # face_class=face_check()
@@ -248,13 +252,13 @@ def register():
 @app.route('/auth')
 def auth():
     var = face_check()
-    print('madhan',var)
+    # print('madhan',var)
     if var==1:
         password1 = open("pass.txt", 'r')
         pass_word = password1.read()
         password1.close()
         if blink()==pass_word:
-            return render_template("dashboard.html")
+            return render_template("auth_sucess.html")
         else:
             return render_template('back.html')
     elif var == 0:
@@ -265,17 +269,120 @@ def auth():
         return render_template('back.html')
 
 
-@app.route('/dash', methods=['GET', 'POST'])
+@app.route('/success', methods=['GET', 'POST'])
 def dash():
-        return render_template('dashboard.html')
+        return render_template('auth_sucess.html')
 
 
 
 
+# def face_check():
+#     model = train_data()
+#     cap = cv2.VideoCapture(0)
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             break  # Exit if the camera feed fails
+
+#         image, face = face_detector(frame)
+#         try:
+#             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+#             results = model.predict(face)
+
+#             if results[1] < 500:
+#                 confidence = int(100 * (1 - (results[1]) / 400))
+#                 cv2.putText(image, f'{confidence}% Confidence', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 120, 150), 2)
+#                 if confidence >= 85:
+#                     cap.release()
+#                     cv2.destroyAllWindows()
+#                     return 1
+#                 else:
+#                     return 2
+#         except Exception as e:
+#             cv2.putText(image, "No Face Found", (220, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+#             print(f"Error: {e}")
+#             return 0
+
+#         cv2.imshow('Face Recognition', image)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+#     return 0
 
 
 
+# def blink():
+#     cap = cv2.VideoCapture(0)
+#     detector = FaceMeshDetector(maxFaces=1)
+#     plotY = LivePlot(640, 360, [20, 50], invert=True)
+#     idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+#     ratioList = []
+#     blinkCounter = 0
+#     counter = 0
+#     color = (255, 0, 255)
+#     delay_key = 0
 
+#     # Load password
+#     with open("pass.txt", 'r') as password_file:
+#         pass_word = password_file.read()
+
+#     fin_key = ''
+#     while True:
+#         success, img = cap.read()
+#         if not success:
+#             break  # Exit if the camera feed fails
+
+#         img, faces = detector.findFaceMesh(img, draw=False)
+#         if faces:
+#             face = faces[0]
+#             for id in idList:
+#                 tuple(cv2.circle(img, face[id], 5, color, cv2.FILLED))
+#             leftUp, leftDown = face[159], face[23]
+#             leftLeft, leftRight = face[130], face[243]
+#             lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+#             lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+
+#             ratio = int((lenghtVer / lenghtHor) * 100)
+#             ratioList.append(ratio)
+#             if len(ratioList) > 3:
+#                 ratioList.pop(0)
+
+#             ratioAvg = sum(ratioList) / len(ratioList)
+#             if ratioAvg < 35 and counter == 0:
+#                 blinkCounter += 1
+#                 delay_key = 0
+#                 color = (0, 200, 0)
+#                 counter = 1
+#             if counter != 0:
+#                 counter += 1
+#                 if counter > 10:
+#                     counter = 0
+#                     color = (0, 0, 255)
+
+#             cvzone.putTextRect(img, f'Blink Count: {blinkCounter}', (50, 100), colorR=color)
+#         else:
+#             cv2.putText(img, "No Face Detected", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+#         delay_key += 1
+#         if delay_key % 30 == 0:  # Approx. 1-second delay
+#             if len(fin_key) == 0 and blinkCounter == 0:
+#                 pass
+#             else:
+#                 fin_key += str(blinkCounter)
+#             blinkCounter = 0
+
+#         if len(fin_key) == len(pass_word):
+#             break
+
+#         cv2.imshow("Image", img)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+#     return fin_key
 
 if __name__=="__main__":
     app.run(debug=True)
